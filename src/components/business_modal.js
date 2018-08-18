045f97715a4b60cb1187b96249fb2069b118c3f6
John Carlisle <jmcarlisle@live.com>
2018-08-18 15:24:08 -0700

    centered modal, lined up and sized map, and added scrool to business description

--- src/components/business_modal.js
+++ src/components/business_modal.js
@@ -41,25 +41,30 @@ class BusinessModal extends Component {
                         <button className='btn red right'>X</button>
                     </div>
                     <div className='row'>   
-                        <div className ="bm-map col s12">
+                        <div className ="bm-map">
                            <GoogleMap />
                         </div>
-                            <ul className="row">
+                            {/* <ul className="row">
                                 <li className='col s3'>Drive Time</li>
                                 <li className='col offset-s1 s3'>No Traffic</li>
                                 <li className='col offset-s1 s3'>15 mins</li>
                                 <li className='col offset-s4 s3'>With Traffic</li>
                                 <li className='col offset-s1 s3'>45 mins</li>
-                            </ul>
+                            </ul> */}
                         
-                        <div className='jobDetails'>
+                        <div className='bm-jobDetails'>
                             <label>Job Description</label>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                            <p className ="bm-jobDescription">Lorem ipsum dolor sit amet consectetur adipisicing elit.
                                 Molestias alias reprehenderit magni illo fugiat. Architecto recusandae 
                                 voluptatem est! Blanditiis culpa sapiente dolor iure itaque at deserunt 
                                 delectus nulla eveniet ad incidunt quia voluptas eos, veniam laborum earum 
                                 modi eum voluptate? Molestias ipsum accusamus repellendus natus vitae numquam 
-                                magni nostrum velit eaque. Recusandae eius odio est?</p>
+                                magni nostrum velit eaque. Recusandae eius odio est? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure saepe odio blanditiis tenetur, ullam, accusantium quia laboriosam eveniet corrupti non pariatur ad, sequi nobis obcaecati perspiciatis fuga dolore error excepturi!
+                                Neque commodi aperiam perspiciatis. Nostrum at quidem eum dolor culpa, adipisci aliquam excepturi in corrupti amet commodi nisi? Quidem facilis neque eos eligendi culpa cum nihil at animi quam voluptates?
+                                Praesentium similique animi eius, sequi, dignissimos quaerat error nisi esse numquam voluptatibus non? Nam fugit, maiores asperiores, perspiciatis neque excepturi quam quia dolores mollitia odio officiis, optio ullam nihil et?
+                                Omnis mollitia hic, quia quaerat unde ut exercitationem fuga, iusto minus dolor, doloribus expedita veritatis? Itaque atque laborum, tempora sit eos molestiae assumenda fugit? Eius ducimus exercitationem dolores? Optio, laborum.
+                                Veritatis iusto rerum omnis, temporibus fuga asperiores expedita accusamus est rem quae natus neque. Est repellat quo eveniet voluptatibus molestiae autem nisi laborum magnam quis, nam, a sapiente incidunt non?
+                                Nihil ducimus dolore alias, beatae magnam asperiores a quae accusamus ad est ratione nam eaque provident? Unde beatae officia, ipsum voluptatum omnis eligendi laboriosam reprehenderit eaque quo, numquam tempore eveniet!        </p>
                         </div>
                     </div>
                 </div>
