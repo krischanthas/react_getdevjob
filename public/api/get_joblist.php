f68ddbd07b2886c4fd82d5436886a2d61893f791
Ryan Kang <shk130@ucsd.edu>
2018-08-23 20:10:49 -0700

    endpoint

--- public/api/get_joblist.php
+++ public/api/get_joblist.php
@@ -4,16 +4,40 @@
         "success"=>false
     ];
     $query = "SELECT * FROM `jobs`";
+    // $query = "SELECT * FROM `jobs` RIGHT JOIN `companies` ON `jobs`.`company_id`=`companies`.`ID`";
     $result = mysqli_query($conn, $query);
     if(mysqli_num_rows($result) > 0){
+        $count = 0;
         while($row = mysqli_fetch_assoc($result)){
-             $output["job"][] = $row;
+            $output["job"][] = $row;
+            $companyID = $row["company_id"];
+            $salaryID = $row["salary_id"];
+            $companyQuery = "SELECT * FROM `companies` WHERE `ID`=$companyID";
+            $companyResult = mysqli_query($conn, $companyQuery);
+            if(mysqli_num_rows($companyResult) > 0){
+                $companyRow = mysqli_fetch_assoc($companyResult);
+                $output["job"][$count]["company"] = $companyRow;
+            }
+            $locationQuery = "SELECT * FROM `locations` WHERE `company_id`=$companyID";
+            $locationResult = mysqli_query($conn, $locationQuery);
+            if(mysqli_num_rows($locationResult) > 0){
+                $locationRow = mysqli_fetch_assoc($locationResult);
+                $output["job"][$count]["company"]["location"] = $locationRow;
+            }
+            $salaryQuery = "SELECT * FROM `salaries` WHERE `ID`=$salaryID";
+            $salaryResult = mysqli_query($conn, $salaryQuery);
+            if(mysqli_num_rows($salaryResult) > 0){
+                $salaryRow = mysqli_fetch_assoc($salaryResult);
+                $output["job"][$count]["company"]["salary"] = $salaryRow;
+            }
+            $count++;
         }
         $output["success"] = true;
     }
     else{
         $ouput["message"] = "fail";
     }
+
 
     $output = json_encode($output);
 
