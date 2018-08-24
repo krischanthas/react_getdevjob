a891235fab60f2b8f607387a717f3d0611834ce6
John Carlisle <jmcarlisle@live.com>
2018-08-24 10:31:51 -0700

    captured all forms values in filter and stored them in state

--- src/components/filters.js
+++ src/components/filters.js
@@ -4,50 +4,95 @@ import {Row, Input} from 'react-materialize';
 
 
 class Filters extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            minSalary:'',
+            maxSalary:'',
+            distance:'',
+            experience:'',
+            postedDate:'',
+            employmentTypeContract: false,
+            employmentTypeInternship: false,
+            employmentTypePartTime: false,
+            employmentTypeFullTime: false,
+        }
+    }
     
+    handleChange(event){
+        debugger;
+
+        const {name, value} = event.currentTarget;
+
+        this.setState({
+            [name]:value
+        })
+       
+
+    }
+    handleCheckBox(event){
+        debugger;
+        console.log("Event:" , event);
+
+        const {name, checked} = event.currentTarget;
+        if(checked !== false){
+            this.setState({
+            [name]:true
+        })
+        }else{
+            this.setState({
+                [name]:false
+            })
+        }
+
+    }
+
+
+
     render(){
         const minSalary = "All Available";
         return (
             <div>
                 <div className ="sidebar">
                     <Row>
-                        <Input s={6} type ='select' label = 'Min Salary' defaultValue = {minSalary}>
+                        <Input s={6} type ='select' label = 'Min Salary' name="minSalary" defaultValue = {minSalary} onChange={this.handleChange.bind(this)}>
                             <option value = '1'> $0</option>
                             <option value = '2'> $30K</option>
                             <option value = '3'> $50K+</option>
                         </Input>
-                        <Input s={6} type ='select' label = 'Max Salary' defaultValue = {minSalary}>
+                        <Input s={6} type ='select' label = 'Max Salary' name='maxSalary'  defaultValue = {minSalary} onChange={this.handleChange.bind(this)} >
                             <option value = '1'> $30K</option>
                             <option value = '2'> $50K</option>
                             <option value = '3'> $70K+</option>
                         </Input>
                     </Row>
                     <Row>
-                        <Input s={12} type ='select' label = 'Distance' defaultValue ='Nearby'>
+                        <Input s={12} type ='select' label = 'Distance' name='distance' defaultValue ='Nearby' onChange={this.handleChange.bind(this)}>
                             <option value = '1'>5 miles</option>
                             <option value = '2'>15 miles</option>
                             <option value = '3'>15+ miles</option>
                         </Input>
                     </Row>
                     <Row>
-                        <Input s={12} type ='select' label = 'Experience' defaultValue ='All Available'>
+                        <Input s={12} type ='select' label = 'Experience' name='experience' defaultValue ='All Available' onChange={this.handleChange.bind(this)}>
                             <option value = '1'>0-2 years</option>
                             <option value = '2'>2-5 years</option>
                             <option value = '3'>5+ years</option>
                         </Input>
                     </Row>
                     <Row>
-                        <Input s={12} type ='select' label = 'Posted Within' defaultValue ='All'>
+                        <Input s={12} type ='select' label = 'Posted Within' name='postedDate' defaultValue ='All' onChange={this.handleChange.bind(this)}>
                             <option value = '1'>7 days</option>
                             <option value = '2'>14 days</option>
                             <option value = '3'>30 days</option>
                         </Input>
                     </Row>
                     <Row>
-                        <Input name='employmentType' type='checkbox' value = 'contract' label='Contract' />
-                        <Input name='employmentType' type='checkbox' value = 'internship' label='Internship' />
-                        <Input name='employmentType' type='checkbox' value = 'partTime' label='Part-Time' />
-                        <Input name='employmentType' type='checkbox' value = 'fullTime' label='Full Time' />
+                        <Input name='employmentTypeContract' type='checkbox' checked={this.state.employmentTypeContract} value = 'contract' label='Contract'  onChange={this.handleCheckBox.bind(this)} />
+                        <Input name='employmentTypeInternship' type='checkbox' checked={this.state.employmentTypeInternship} value = 'internship' label='Internship'  onChange={this.handleCheckBox.bind(this)} />
+                        <Input name='employmentTypePartTime' type='checkbox' checked={this.state.employmentTypePartTime} value = 'partTime' label='Part-Time'  onChange={this.handleCheckBox.bind(this)}/>
+                        <Input name='employmentTypeFullTime' type='checkbox' checked={this.state.employmentTypeFullTime} value = 'fullTime' label='Full Time'  onChange={this.handleCheckBox.bind(this)}/>
                     </Row>
                 </div>
             </div>
